cb6e8d44f324ee19aeb353cf141903e206118ca7
27MarioGomez <mariogomezballesteros@hotmail.com>
2024-12-12 19:52:25 +0100

    update repo

--- Project_Break_I__EDA/dashboard.py
+++ Project_Break_I__EDA/dashboard.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Cargar datos
 @st.cache_data
 def load_data():
-    filepath = "src/data/champions_league_complete_with_rounds.csv"
+    filepath = "EDA - CHAMPIONS LEAGUE/src/data/champions_league_complete_with_rounds.csv"
     return pd.read_csv(filepath)
 
 df = load_data()
